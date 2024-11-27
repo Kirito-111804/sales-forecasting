4d211be50ca49aa79fcb06c3fe410a9b614295c6
Avila2203397 <avilajohnandrei97@gmail.com>
2024-11-27 19:59:59 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import FileUploader from './FileUploader';
+import SalesForecasting from './SalesForecasting';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1 style={{ textAlign: 'center' }}>Sales Forecasting</h1>
+      <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+        {!data && <FileUploader onDataProcessed={setData} />}
+        {data && <SalesForecasting data={data} />}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
